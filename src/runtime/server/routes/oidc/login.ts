b37ab7fc881165270c587f33d51e7751708730cd
GlennDutrieux <glenn.dutrieux@hawking.health>
2023-04-11 14:08:11 +0200

    v1.0.3

--- src/runtime/server/routes/oidc/login.ts
+++ src/runtime/server/routes/oidc/login.ts
@@ -1,53 +1,81 @@
-import { defineEventHandler, setCookie, getCookie } from 'h3'
-import { v4 as uuidv4 } from 'uuid'
-import { generators } from 'openid-client'
-import { initClient } from '../../../utils/issueclient'
-import { logger } from '../../../utils/logger'
-import { getRedirectUrl, getCallbackUrl, getDefaultBackUrl, getResponseMode } from '../../../utils/utils'
-import { useRuntimeConfig } from '#imports'
+import { defineEventHandler, setCookie, getCookie } from "h3";
+import { v4 as uuidv4 } from "uuid";
+import { generators } from "openid-client";
+import { initClient } from "../../../utils/issueclient";
+import { logger } from "../../../utils/logger";
+import {
+  getRedirectUrl,
+  getCallbackUrl,
+  getDefaultBackUrl,
+  getResponseMode,
+} from "../../../utils/utils";
+import { useRuntimeConfig } from "#imports";
 
 export default defineEventHandler(async (event) => {
-  logger.info('[Login]: oidc/login calling')
-  const req = event.node.req
-  const res = event.node.res
+  logger.info("[Login]: oidc/login calling");
+  const req = event.node.req;
+  const res = event.node.res;
 
-  const { op, config } = useRuntimeConfig().openidConnect
-  const redirectUrl = getRedirectUrl(req.url)
-  const callbackUrl = getCallbackUrl(op.callbackUrl, redirectUrl, req.headers.host)
-  const defCallBackUrl = getDefaultBackUrl(redirectUrl, req.headers.host)
+  const { op, config } = useRuntimeConfig().openidConnect;
+  const redirectUrl = getRedirectUrl(req.url);
+  const callbackUrl = getCallbackUrl(
+    op.callbackUrl,
+    redirectUrl,
+    req.headers.host,
+    op.useSsl
+  );
+  const defCallBackUrl = getDefaultBackUrl(
+    redirectUrl,
+    req.headers.host,
+    op.useSsl
+  );
 
-  const issueClient = await initClient(op, req, [defCallBackUrl, callbackUrl])
-  const sessionkey = config.secret
-  let sessionid = getCookie(event, config.secret)
+  const issueClient = await initClient(op, req, [defCallBackUrl, callbackUrl]);
+  const sessionkey = config.secret;
+  let sessionid = getCookie(event, config.secret);
   if (!sessionid) {
-    sessionid = generators.nonce()
-    logger.info('[Login]: regenerate sessionid=' + sessionid)
+    sessionid = generators.nonce();
+    logger.info("[Login]: regenerate sessionid=" + sessionid);
   } else {
-    logger.info('[Login]: cookie sessionid=' + sessionid)
+    logger.info("[Login]: cookie sessionid=" + sessionid);
   }
 
-  const responseMode = getResponseMode(config)
-  const scopes = op.scope.includes('openid') ? op.scope : [...op.scope, 'openid']
-  logger.info('[Login]: cabackurl & op.callbackUrl & redirecturl: ', callbackUrl, op.callbackUrl, redirectUrl)
-  logger.info('  response_mode:' + responseMode + ', response_type:' + config.response_type + ', scopes:' + scopes.join(' '))
+  const responseMode = getResponseMode(config);
+  const scopes = op.scope.includes("openid")
+    ? op.scope
+    : [...op.scope, "openid"];
+  logger.info(
+    "[Login]: cabackurl & op.callbackUrl & redirecturl: ",
+    callbackUrl,
+    op.callbackUrl,
+    redirectUrl
+  );
+  logger.info(
+    "  response_mode:" +
+      responseMode +
+      ", response_type:" +
+      config.response_type +
+      ", scopes:" +
+      scopes.join(" ")
+  );
 
   const parameters = {
     redirect_uri: callbackUrl,
     response_type: config.response_type,
     response_mode: responseMode,
     nonce: sessionid,
-    scope: scopes.join(' ')
-  }
-  const authUrl = issueClient.authorizationUrl(parameters)
-  logger.info('[Login]: Auth Url: ' + authUrl + ',    #sessionid:' + sessionid)
+    scope: scopes.join(" "),
+  };
+  const authUrl = issueClient.authorizationUrl(parameters);
+  logger.info("[Login]: Auth Url: " + authUrl + ",    #sessionid:" + sessionid);
 
   if (sessionid) {
     setCookie(event, sessionkey, sessionid, {
       maxAge: config.cookieMaxAge,
-      ...config.cookieFlags[sessionkey as keyof typeof config.cookieFlags]
-    })
+      ...config.cookieFlags[sessionkey as keyof typeof config.cookieFlags],
+    });
   }
 
-  res.writeHead(302, { Location: authUrl })
-  res.end()
-})
+  res.writeHead(302, { Location: authUrl });
+  res.end();
+});
--- src/runtime/utils/utils.ts
+++ src/runtime/utils/utils.ts
@@ -1,69 +1,76 @@
 export const isUnset = (o: unknown): boolean =>
-  typeof o === 'undefined' || o === null
+  typeof o === "undefined" || o === null;
 
-export const isSet = (o: unknown): boolean => !isUnset(o)
+export const isSet = (o: unknown): boolean => !isUnset(o);
 
 export const getRedirectUrl = (uri: string | null | undefined): string => {
   if (!uri) {
-    return '/'
+    return "/";
   }
-  const idx = uri.indexOf('?')
-  const searchParams = new URLSearchParams(idx >= 0 ? uri.substring(idx) : uri)
-  return searchParams.get('redirect') || '/'
-}
+  const idx = uri.indexOf("?");
+  const searchParams = new URLSearchParams(idx >= 0 ? uri.substring(idx) : uri);
+  return searchParams.get("redirect") || "/";
+};
 
-export function getCallbackUrl(callbackUrl: string, redirectUrl: string, host: string | undefined, useSsl: boolean | false): string {
-  if ((callbackUrl && callbackUrl.length > 0)) {
-    return callbackUrl.includes('?') ? (callbackUrl + '&redirect=' + redirectUrl) : (callbackUrl + '?redirect=' + redirectUrl)
+export function getCallbackUrl(
+  callbackUrl: string,
+  redirectUrl: string,
+  host: string | undefined,
+  useSsl: boolean | false
+): string {
+  if (callbackUrl && callbackUrl.length > 0) {
+    return callbackUrl.includes("?")
+      ? callbackUrl + "&redirect=" + redirectUrl
+      : callbackUrl + "?redirect=" + redirectUrl;
   } else {
-    return getDefaultBackUrl(redirectUrl, host, useSsl)
+    return getDefaultBackUrl(redirectUrl, host, useSsl);
   }
 }
 
 export function getDefaultBackUrl(redirectUrl, host, useSsl) {
   if (useSsl) {
     return "https://" + host + "/oidc/cbt?redirect=" + redirectUrl;
-  } 
-  
-  return "http://" + host + "/oidc/cbt?redirect=" + redirectUrl; 
+  }
+
+  return "http://" + host + "/oidc/cbt?redirect=" + redirectUrl;
 }
 
 /**
-   * Response Mode
-   *   The Response Mode determines how the Authorization Server returns result parameters from the Authorization Endpoint.
-   *   Non-default modes are specified using the response_mode request parameter. If response_mode is not present in a request, the default Response Mode mechanism specified by the Response Type is used.
-   *
-   * REF:
-   *   https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html (English)
-   *   https://linianhui.github.io/authentication-and-authorization/05-openid-connect-extension/  (Chinese)
-   *
-   * response_mode
-   * 1. query
-   *    In this mode, Authorization Response parameters are encoded in the query string added to the redirect_uri when redirecting back to the Client.
-   *    callback http.method: 'GET'
-   *    example: https://client.lnh.dev/oauth2-callback?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz
-   * 2. fragment
-   *    In this mode, Authorization Response parameters are encoded in the fragment added to the redirect_uri when redirecting back to the Client.
-   *    callback http.method: 'GET'
-   *    example: http://client.lnh.dev/oauth2-callback#access_token=2YotnFZFEjr1zCsicMWpAA&state=xyz&expires_in=3600
-   * 3. form_post (Callback method 'POST') (REF: https://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html)
-   *
-   * Hints:
-   * i)   For purposes of this specification, the default Response Mode for the OAuth 2.0 code Response Type is the query encoding.
-   * ii)  For purposes of this specification, the default Response Mode for the OAuth 2.0 token Response Type is the fragment encoding.
-   * iii) Response_mode 'query' not allowed for implicit or hybrid flow
-   */
+ * Response Mode
+ *   The Response Mode determines how the Authorization Server returns result parameters from the Authorization Endpoint.
+ *   Non-default modes are specified using the response_mode request parameter. If response_mode is not present in a request, the default Response Mode mechanism specified by the Response Type is used.
+ *
+ * REF:
+ *   https://openid.net/specs/oauth-v2-multiple-response-types-1_0.html (English)
+ *   https://linianhui.github.io/authentication-and-authorization/05-openid-connect-extension/  (Chinese)
+ *
+ * response_mode
+ * 1. query
+ *    In this mode, Authorization Response parameters are encoded in the query string added to the redirect_uri when redirecting back to the Client.
+ *    callback http.method: 'GET'
+ *    example: https://client.lnh.dev/oauth2-callback?code=SplxlOBeZQQYbYS6WxSbIA&state=xyz
+ * 2. fragment
+ *    In this mode, Authorization Response parameters are encoded in the fragment added to the redirect_uri when redirecting back to the Client.
+ *    callback http.method: 'GET'
+ *    example: http://client.lnh.dev/oauth2-callback#access_token=2YotnFZFEjr1zCsicMWpAA&state=xyz&expires_in=3600
+ * 3. form_post (Callback method 'POST') (REF: https://openid.net/specs/oauth-v2-form-post-response-mode-1_0.html)
+ *
+ * Hints:
+ * i)   For purposes of this specification, the default Response Mode for the OAuth 2.0 code Response Type is the query encoding.
+ * ii)  For purposes of this specification, the default Response Mode for the OAuth 2.0 token Response Type is the fragment encoding.
+ * iii) Response_mode 'query' not allowed for implicit or hybrid flow
+ */
 export function getResponseMode(config: any): string {
-  const responseType = config.response_type
-  return config.response_mode || getDefaultResponseMode(responseType)
+  const responseType = config.response_type;
+  return config.response_mode || getDefaultResponseMode(responseType);
 }
 
 function getDefaultResponseMode(responseType: string): string {
-  const resTypeArray = responseType.match(/[^ ]+/g)
-  if (resTypeArray && resTypeArray?.findIndex(i => i === 'code') >= 0) {
-    return 'query'
-  } else if (resTypeArray && resTypeArray?.findIndex(i => i === 'token')) {
-    return 'fragment'
+  const resTypeArray = responseType.match(/[^ ]+/g);
+  if (resTypeArray && resTypeArray?.findIndex((i) => i === "code") >= 0) {
+    return "query";
+  } else if (resTypeArray && resTypeArray?.findIndex((i) => i === "token")) {
+    return "fragment";
   }
-  return 'query'
+  return "query";
 }
